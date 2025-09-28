686541d6d1a1803805a8f4fbf86d64fc4c67bed1
Santiago Gauna <santiagogauna@192.168.1.5>
2025-09-28 12:01:11 -0300

    Fix: Vite base path and deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/Mi-Portafolio/"
+  base: "/Mi-Portafolio/", // 👈 tiene que coincidir EXACTO con el nombre del repo en GitHub
 })
