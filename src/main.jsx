4920a3c611ca62ef99aeacc464bc33746714f64b
Santiago Gauna <santiagogauna@192.168.1.5>
2025-10-05 12:13:15 -0300

    Comiquito

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import classes from './Main.module.css'
+import "./Main.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className={classes.background}>
       <App />
-    </div>
   </StrictMode>,
 )
 
